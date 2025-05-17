c732ac355f33cfc149a655d94e6126f775a2891d
gabrielleal23 <glealhe01478@universidadean.edu.co>
2025-05-16 22:07:33 -0500

    Actualizacion visual modulo admin

--- app/src/main/java/com/example/conectandoapp/AdminActivity.kt
+++ app/src/main/java/com/example/conectandoapp/AdminActivity.kt
@@ -3,6 +3,7 @@ package com.example.conectandoapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,10 +12,11 @@ class AdminActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
 
-        val btnUsuarios: Button = findViewById(R.id.btnUsuariosadmin)
-        val btnPostulaciones: Button = findViewById(R.id.btnPostulaciones)
-        val btnOfertas: Button = findViewById(R.id.btnOfertas)
+        val btnUsuarios: LinearLayout = findViewById(R.id.btnUsuariosadmin)
+        val btnPostulaciones: LinearLayout = findViewById(R.id.btnPostulaciones)
+        val btnOfertas: LinearLayout = findViewById(R.id.btnOfertas)
         val btnCerrarSesion: Button = findViewById(R.id.btnCerrarSesion)
+        val btnAdminProfile: LinearLayout = findViewById(R.id.btnAdminProfile)
 
         btnUsuarios.setOnClickListener {
             startActivity(Intent(this, UserManagementActivity::class.java))
@@ -26,6 +28,10 @@ class AdminActivity : AppCompatActivity() {
 
         btnOfertas.setOnClickListener {
             startActivity(Intent(this, MentorsAdminActivity::class.java))
+        }
+
+        btnAdminProfile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
         }
 
         btnCerrarSesion.setOnClickListener {
